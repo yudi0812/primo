f5d3bad2d593e0cffde3eac2299fcaf1976488cc
Yogesh <hacker0701@gmail.com>
2024-02-10 11:18:55 +0530

    changed token

--- src/routes/api/deploy/repos/+server.js
+++ src/routes/api/deploy/repos/+server.js
@@ -11,9 +11,45 @@ export async function GET({ locals, url }) {
 
   const provider = url.searchParams.get('provider')
 
-  const headers = {
-  	Authorization: `Bearer ${token?.value}`,
-  	Accept: 'application/vnd.github.v3+json'
+  const { data: token } = await supabase_admin
+    .from('config')
+    .select('value')
+    .eq('id', `${provider}_token`)
+    .single()
+
+  let repos = null
+  if (provider === 'github' && token) {
+    const headers = {
+      Authorization: `Bearer ${token.value}`,
+      Accept: 'application/vnd.github.v3+json',
+    }
+
+    const res = await Promise.all([
+      axios.get(`https://api.github.com/user/repos?per_page=100`, {
+        headers: { ...headers },
+      }),
+      axios.get(`https://api.github.com/user/repos?per_page=100&page=2`, {
+        headers: { ...headers },
+      }),
+    ]).then((res) => res.map(({ data }) => data))
+
+    repos = res.flat().map((repo) => ({
+      id: repo.full_name,
+      label: repo.name,
+    }))
+  } else if (provider === 'gitlab' && token) {
+    const res = await axios.get('https://gitlab.com/api/v4/projects', {
+      headers: { Authorization: `Bearer ${token.value}` },
+      params: {
+        owned: true,
+        per_page: 100,
+      },
+    })
+
+    repos = res.data?.map((project) => ({
+      id: project.path_with_namespace,
+      label: project.name,
+    }))
   }
 
   return json(repos)
